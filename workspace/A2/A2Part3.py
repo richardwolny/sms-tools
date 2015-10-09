2269412a267d2ab0e69056e77f9f4e85c245af8b
Richard Wolny <richard.wolny@gmail.com>
2015-10-09 18:33:35 -0500

    Part3

--- workspace/A2/A2Part3.py
+++ workspace/A2/A2Part3.py
@@ -26,3 +26,13 @@ def DFT(x):
         X (numpy array) = The N point DFT of the input sequence x
     """
     ## Your code here
+    N = x.size
+    nv = np.arange(N)
+    X = np.array([])
+
+    for k in nv:
+        s = np.exp(1j * 2 * np.pi * k / N * nv)
+        X = np.append(X, sum(x * np.conjugate(s)))
+
+    return X
+
